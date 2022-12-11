88e34e56a173fa4eefa0908536a625742188261e
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-11 10:03:13 +0200

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,13 @@
+let number = 0;
+
+for (let i = 100; i <= 999; i++){
+    number = i;
+    let firstD = Math.floor(number/100);
+    let secondD = Math.floor((number % 100)/10);
+    let y = number % 100;
+    let thirdD = y % 10;
+
+    if (firstD !== secondD && firstD !== thirdD && secondD !== thirdD){
+        console.log(number);
+    }
+}
