f533a680d9dceba077dc0f1246358bb4c1f90b5b
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-11 11:32:52 +0200

    Create 17.js

---
+++ 17.js
@@ -0,0 +1,23 @@
+let side = 6;
+let sign = "/";
+
+for(let i = 1; i <= side; i++){
+    if (i === 1 || i === side){
+        let line = '';
+        for (let j = 1; j <= side; j++){
+        line += '*';
+    }
+    console.log(line);
+    } else {
+        let midLine = '';
+        for(let k = 1; k <= side; k++){
+            if(k === 1 || k === side){
+                midLine += '*';
+            } else {
+                midLine += sign;
+            }
+        }
+        console.log(midLine);
+    }
+}
+
