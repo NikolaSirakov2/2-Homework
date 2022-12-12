b18e8a2af70f3401cd37d0c7cb822c5efa2b9798
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-12 20:33:27 +0200

    Create 21.js

---
+++ 21.js
@@ -0,0 +1,39 @@
+let n = 47;
+let cards = ""
+
+for (let i = n; i <= 51; i++) {
+  let color;
+
+  let sum = Math.floor(i / 4);
+  let modularDivision = i % 4;
+
+  if (modularDivision === 0) {
+    sum += 1;
+    color = "пика";
+  } else if (modularDivision === 1) {
+    sum += 2;
+    color = "спатия";
+  } else if (modularDivision === 2) {
+    sum += 2;
+    color = "каро";
+  } else if (modularDivision === 3) {
+    sum += 2;
+    color = "купа";
+  }
+
+  if (sum < 11) {
+    cards += sum, color;
+  } else if (sum === 11) {
+    cards += "J", color;
+  } else if (sum === 12) {
+    cards += "Q", color;
+  } else if (sum === 13) {
+    cards += `Поп ${color}, `;
+  } else if (sum === 14) {
+    cards += `Aсо ${color}, `;
+  }
+}
+
+cards += "Aco пика";
+
+console.log(cards);
