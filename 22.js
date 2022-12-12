267057cc07a43671f2660c5275167ae30434f0c6
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-12 10:48:12 +0200

    Update 22.js

--- 22.js
+++ 22.js
@@ -2,19 +2,17 @@ let n = 1;
 let count = 0;
 let numbers = "";
 
-
-  for (let i = n + 1; i <= 1100; i++) {
+while (count < 11) {
+  for (let i = n + 1; i <= 1050; i++) {
     if (i % 2 === 0 || i % 3 === 0 || i % 5 === 0) {
       count++;
-      if(count === 11){
-        break;
-      } else if (count !== 10) {
+      if (count !== 10) {
         numbers += `${count}:${i},`;
       } else if (count === 10) {
         numbers += `${count}:${i}`;
       }
     }
   }
-
+}
 
 console.log(numbers);
