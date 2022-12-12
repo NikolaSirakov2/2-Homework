f8771a982e8d5e5d74903b59af47d6324527373d
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-12 12:08:34 +0200

    Create luki.js

---
+++ luki.js
@@ -0,0 +1,13 @@
+let side = 6;
+
+for ( a=0; a<=side; a++){
+    if(a===1){
+        console.log ("****");
+    } else if (a===2){
+        console.log("*++*");
+    } else if (a===3){
+        console.log ("*++*");
+    } else if (a===4){
+        console.log ("****")
+    }
+}
