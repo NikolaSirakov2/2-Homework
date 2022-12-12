344b3e418a67e6ba04a1bc350736b2639657b004
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-12 09:47:10 +0200

    Create 19.js

---
+++ 19.js
@@ -0,0 +1,18 @@
+let num = 11;
+let numbers = "";
+
+while (num !== 1) {
+  if (num % 2 === 0) {
+    num = 0.5 * num;
+    if (num === 1) {
+      numbers += num + ".";
+    } else {
+      numbers += num + " ";
+    }
+  } else {
+    num = 3 * num + 1;
+    numbers += num + " ";
+  }
+}
+
+console.log(numbers);
