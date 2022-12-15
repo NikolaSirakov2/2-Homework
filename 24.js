9f13433e1cce44f2fcf60702e575625fbb21338b
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-15 11:11:43 +0200

    Update 24.js

--- 24.js
+++ 24.js
@@ -1,5 +1,15 @@
-let num = 23132;
+let num = 23431;
+let palindrome = num;
+let turnNum = 0;
 
-do {
-    num % 10
-} while (num === 0)
+while (palindrome > 0) {
+  let remainder = Math.floor(palindrome % 10);
+  turnNum = Math.floor(turnNum * 10 + remainder);
+  palindrome = Math.floor(palindrome / 10);
+}
+
+if (num === turnNum) {
+  console.log(`${num} is palindrome`);
+} else {
+  console.log(`${num} is not palindrome`);
+}
