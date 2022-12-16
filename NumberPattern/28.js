8d252506f48ac8f9eff060f0bff8ebd16d222042
Nikola Sirakov <118350670+NikolaSirakov2@users.noreply.github.com>
2022-12-16 10:29:17 +0200

    Create 28.js

---
+++ NumberPattern/28.js
@@ -0,0 +1,14 @@
+let num = 5;
+let square = "";
+
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= num; j++) {
+    while (j <= i){
+    square += num - j + 1 + " ";
+    break;
+    }
+  }
+  square += "\n";
+}
+
+console.log(square);
